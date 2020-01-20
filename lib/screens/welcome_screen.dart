c23d0939f19d349d78008b3bead7ae418fb451c4
angre-kim <angre@daum.net>
2020-01-20 13:05:29 +0900

    animation 커스텀화-적용된 화면

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -28,13 +28,16 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
     controller.forward();
 
     controller.addListener((){
+      setState(() {
+
+      });
       print(controller.value);
     }    );
       }
      @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.red.withOpacity(controller.value),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(

58e84111714ef32330a8609fa45903198ea425d1
angre-kim <angre@daum.net>
2020-01-22 15:04:18 +0900

    tween animation 최종

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -19,8 +19,8 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     super.initState();
 
     controller =
-        AnimationController(duration: Duration(seconds: 3), vsync: this);
-    animation = ColorTween(begin: Colors.red, end: Colors.blue).animate(controller);
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white).animate(controller);
     controller.forward();
 
     controller.addListener(() {
